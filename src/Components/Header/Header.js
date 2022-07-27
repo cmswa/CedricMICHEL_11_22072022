2dcd0553aaa722fa91c014f08c5e2228a05dab68
Cédric Michel <c-e-d@live.fr>
2022-07-27 14:09:41 +0200

    nav activeLink underline

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,15 +2,25 @@ import React from 'react'
 import Logo from '../../assets/icons/LOGO.png'
 import './Header.css'
 import { v4 as uuidv4 } from 'uuid';
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className='header'>
       <Link to="/"><img src={Logo} alt="Kasa" /></Link>
       <nav className='nav'>
-        <Link className="nav-home" to='/'>Accueil</Link>
-        <Link className="nav-about" to='/about'>A Propos</Link>
+        {/* <Link className="nav-home" to='/'>Accueil</Link>
+        <Link className="nav-about" to='/about'>A Propos</Link> */}
+        <NavLink to='/'
+          className={({ isActive }) => isActive ? 'activeLink' : 'notAvtiveLink'}
+        >
+          Accueil
+        </NavLink>
+        <NavLink to='/about'
+          className={({ isActive }) => isActive ? 'activeLink' : 'notAvtiveLink'}
+        >
+          A Propos
+        </NavLink>
       </nav>
 
     </header>
