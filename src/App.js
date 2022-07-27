abc495a53bb2acdce6a24cffaea785165bda9b65
Cédric Michel <c-e-d@live.fr>
2022-07-28 01:09:56 +0200

    LocationsHome - paramètres des routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import NotFound from './pages/Notfound';
 import About from './pages/About';
+import Location from './pages/Location';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='*' element={<NotFound />} />
         <Route path='about' element={<About />} />
+        <Route path='location' element={<Location />} >
+          <Route path=':locationId' element={<Location />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/Components/LocationsHome/LocationsHome.js
+++ src/Components/LocationsHome/LocationsHome.js
@@ -2,7 +2,8 @@ import React from 'react'
 import './LocationsHome.css'
 import { useState, useEffect } from 'react'
 import Data from '../../data/logements.json'
-let url = "";
+// let url = "";
+import { Link } from "react-router-dom";
 
 export default function Gallery() {
 
@@ -11,12 +12,12 @@ export default function Gallery() {
       <ul className="locationsHome__contains">
         {Data.map((logement) => (
           <li className="location" key={logement.id}>
-            <a href={url} className="location__link">
+            <Link to={`/location/${logement.id}`} className="location__link">
               <img src={logement.cover} alt={logement.title} className="location__img" />
               <div className="location__layer"></div>
               <p className="location__title">{logement.title}</p>
 
-            </a>
+            </Link>
 
           </li>
         ))}
