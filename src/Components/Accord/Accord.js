3c787d76b124bccda51124a26bb4c83116d2b1ae
Cédric Michel <c-e-d@live.fr>
2022-07-27 17:14:23 +0200

    accordéon animation

--- src/Components/Accord/Accord.js
+++ src/Components/Accord/Accord.js
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './Accord.css'
 import ChevronDown from '../../assets/icons/chevron-down.png'
 
 export default function Accord() {
+
+  const [toggle, setToggle] = useState(false)
+  const [heightEl, setHeightEl] = useState()
+
+  const toggleState = () => {
+    setToggle(!toggle);
+  }
+
+  const refHeight = useRef();
+
+  useEffect(() => {
+    setHeightEl(`${refHeight.current.scrollHeight}px`)
+  }, [])
+
+
   return (
     <div className='accord'>
 
-      <div className="accord-visible">
+      <div
+        onClick={toggleState}
+        className="accord-visible">
         <h2 className='accordTitle'>Lorem ipsum dolor sit amet.</h2>
-        <img src={ChevronDown} alt="chevron down" />
+        <img className={toggle ? 'chevron rotate' : 'chevron'} src={ChevronDown} alt="chevron down" />
       </div>
 
-      <div className="accord-toogle">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, explicabo. Delectus perferendis earum doloribus quasi mollitia tempore rem molestiae officiis soluta repellendus nobis error, nisi vel provident! Eaque, maxime possimus.</p>
+      <div
+        ref={refHeight}
+        className={toggle ? 'accord-toggle animated' : 'accord-toggle'}
+        style={{ height: toggle ? `${heightEl}` : '0px' }} /* hauteur qui grandit et rétrécit */
+      >
+        <p
+          aria-hidden={toggle ? 'true' : 'false'} /* retirer texte présent à la fermeture */
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, explicabo.
+          Delectus perferendis earum doloribus quasi mollitia tempore rem molestiae
+          officiis soluta repellendus nobis error, nisi vel provident! Eaque, maxime possimus.</p>
       </div>
 
     </div>
