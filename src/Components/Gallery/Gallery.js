a9f8692c85caca1c9203f99078adf5ec49061e05
Cédric Michel <c-e-d@live.fr>
2022-07-29 16:29:11 +0200

    changement d'img Gallery

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -7,14 +7,19 @@ import { useState } from 'react'
 
 export default function Gallery({ pictures, title }) {
 
+  //à l'arrivée sur la page d'un logement, currentPicture est initialisé à 0
   const [currentPicture, setCurrentPicture] = useState(0)
+  const picturesLength = pictures.length
+
+  const nextPicture = () => { setCurrentPicture(currentPicture === picturesLength - 1 ? 0 : currentPicture + 1) }
+  const previousPicture = () => { setCurrentPicture(currentPicture === 0 ? picturesLength - 1 : currentPicture - 1) }
 
   return (
     <section className="gallery">
       {/* checker alt après un changement de photos */}
       <img className='gallery__img' src={pictures[currentPicture]} alt={title} />
-      <img src={ChevronLeft} alt="previous" className="chevronLeft" />
-      <img src={ChevronRight} alt="next" className="chevronRight" />
+      <img src={ChevronLeft} alt="previous" className="chevronLeft" onClick={previousPicture} />
+      <img src={ChevronRight} alt="next" className="chevronRight" onClick={nextPicture} />
       <p className="gallery__index">1/6 </p>
     </section>
   )
