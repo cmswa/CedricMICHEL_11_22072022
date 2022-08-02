fcef3663de66c47b4303b90e204fa217ae1be0b9
Cédric Michel <c-e-d@live.fr>
2022-08-02 12:51:52 +0200

    responsive mobile - page Home + header + footer

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Logo from '../../assets/icons/LOGO.png'
 import './Header.css'
-import { v4 as uuidv4 } from 'uuid';
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className='header'>
-      <Link to="/"><img src={Logo} alt="Kasa" /></Link>
+      <Link className='header__a' to="/"><img src={Logo} className="header__logo" alt="Kasa" /></Link>
       <nav className='nav'>
         {/* <Link className="nav-home" to='/'>Accueil</Link>
         <Link className="nav-about" to='/about'>A Propos</Link> */}
--- src/Components/HomeBanner/HomeBanner.js
+++ src/Components/HomeBanner/HomeBanner.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import HomeBannerImg from '../../assets/img/home.png'
+import HomeBannerImgMobile from '../../assets/img/homeMob.png'
 import './HomeBanner.css'
 
 export default function Banner() {
   return (
     <section className="homeBanner">
-        <img src={HomeBannerImg} alt="bannière" className="homeBanner__img" />
-        <div className="homeBanner__layer"></div>
-        <h2 className="homeBanner__txt">
+      <img src={HomeBannerImg} alt="bannière" className="homeBanner__img" />
+      <img src={HomeBannerImgMobile} alt="bannière" className="homeBanner__img--mobile" />
+      <div className="homeBanner__layer"></div>
+      <h2 className="homeBanner__txt">
         Chez vous, partout et ailleurs
-        </h2>
+      </h2>
     </section>
   )
 }
