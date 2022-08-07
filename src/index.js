749af7dd1476ea5eac0b7f67f1115349cee1cef1
Cédric Michel <c-e-d@live.fr>
2022-08-07 17:38:17 +0200

    police ajoutée au projet

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import 'typeface-montserrat';
-<link href="https://fr.allfont.net/allfont.css?fonts=montserrat" rel="stylesheet" type="text/css" />
+import "@fontsource/montserrat";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
